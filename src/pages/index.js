fe5faa5078c1c88965d0c03be011afcc09aca0da
compliment <nishkumar1@outlook.com>
2020-04-20 03:56:54 +0530

    Responsive images on frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,12 @@ import { Container } from "react-bootstrap"
 import Carousel from "react-bootstrap/Carousel"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ImgA from "../images/gallery/Chem.BNMU14@2x.jpg"
-import ImgB from "../images/gallery/Chem.BNMU55@2x.jpg"
-import ImgC from "../images/gallery/Chem.BNMU36@2x.jpg"
+import ImgA from "../images/gallery/Chem.BNMU14@2xx.jpg"
+import ImgB from "../images/gallery/Chem.BNMU55@2xx.jpg"
+import ImgC from "../images/gallery/Chem.BNMU36@2xx.jpg"
+import ImgAb from "../images/gallery/Chem.BNMU14@2x.jpg"
+import ImgBb from "../images/gallery/Chem.BNMU55@2x.jpg"
+import ImgCb from "../images/gallery/Chem.BNMU36@2x.jpg"
 
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "Home" }}>
@@ -18,13 +21,25 @@ const IndexPage = () => (
     <Container>
       <Carousel>
         <Carousel.Item>
-          <img src={ImgA} class="img-fluid lazy" alt="" />
+        <picture>
+    <source media="(max-width: 600px)" srcset={ImgA} />
+    <source media="(min-width: 601px)" srcset={ImgAb} />
+    <img class="img-fluid lazy" src={ImgAb} alt="" />
+    </picture>
         </Carousel.Item>
         <Carousel.Item>
-          <img class="img-fluid lazy" src={ImgB} alt="" />
+        <picture>
+    <source media="(max-width: 600px)" srcset={ImgB} />
+    <source media="(min-width: 601px)" srcset={ImgBb} />
+    <img class="img-fluid lazy" src={ImgBb} alt="" />
+    </picture>
         </Carousel.Item>
         <Carousel.Item>
-          <img class="img-fluid lazy" src={ImgC} alt="" />
+        <picture>
+    <source media="(max-width: 600px)" srcset={ImgC} />
+    <source media="(min-width: 601px)" srcset={ImgCb} />
+    <img class="img-fluid lazy" src={ImgCb} alt="" />
+    </picture>
         </Carousel.Item>
       </Carousel>
     </Container>
