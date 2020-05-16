3358cd59618e52d2bb6930b90b4f453ef9696293
compliment <nishkumar1@outlook.com>
2020-05-16 19:54:11 +0530

    add /

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -28,63 +28,63 @@ const CustomNavbar = ({ pageInfo }) => {
             id="responsive-navbar-nav"
           >
             <Nav className="mr-auto" activeKey={pageInfo && pageInfo.pageName}>
-              <Link to="/about">
+              <Link to="/about/">
                 <Nav.Link as="span" eventKey="about">
                   <Button variant="secondary" active>
                     About
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/alaschem">
+              <Link to="/alaschem/">
                 <Nav.Link as="span" eventKey="ALASCHEM">
                   <Button variant="secondary" active>
                     Alumni
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/faculty">
+              <Link to="/faculty/">
                 <Nav.Link as="span" eventKey="Faculty">
                   <Button variant="secondary" active>
                     Faculty
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/statistics">
+              <Link to="/statistics/">
                 <Nav.Link as="span" eventKey="Statistics">
                   <Button variant="secondary" active>
                     Statistics
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/syllabus">
+              <Link to="/syllabus/">
                 <Nav.Link as="span" eventKey="Syllabus">
                   <Button variant="secondary" active>
                     Syllabus
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/NAAC">
+              <Link to="/NAAC/">
                 <Nav.Link as="span" eventKey="NAAC">
                   <Button variant="secondary" active>
                     NAAC
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/reading-material">
+              <Link to="/reading-material/">
                 <Nav.Link as="span" eventKey="ReadingMaterial">
                   <Button variant="secondary" active>
                     Reading Material
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/photo-gallery">
+              <Link to="/photo-gallery/">
                 <Nav.Link as="span" eventKey="PhotoGallery">
                   <Button variant="secondary" active>
                     Photo Gallery
                   </Button>
                 </Nav.Link>
               </Link>
-              <Link to="/notice">
+              <Link to="/notice/">
                 <Nav.Link as="span" eventKey="Notice">
                   <Button variant="secondary" active>
                     Notice
